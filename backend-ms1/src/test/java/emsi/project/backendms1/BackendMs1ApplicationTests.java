c43821ecb36f614bc54c99167120a5de610bc94c
yassir2222 <yassir.lambrass@gmail.com>
2025-05-07 14:11:19 +0100

    Configure tests to use H2 database

--- backend-ms1/src/test/java/emsi/project/backendms1/BackendMs1ApplicationTests.java
+++ backend-ms1/src/test/java/emsi/project/backendms1/BackendMs1ApplicationTests.java
@@ -1,13 +1,25 @@
 package emsi.project.backendms1;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendMs1ApplicationTests {
+
+    @Autowired
+    private Environment env; // Autowire Environment
 
     @Test
     void contextLoads() {
+        System.out.println("---- TEST DATASOURCE PROPERTIES ----");
+        System.out.println("spring.datasource.url=" + env.getProperty("spring.datasource.url"));
+        System.out.println("spring.datasource.driverClassName=" + env.getProperty("spring.datasource.driverClassName"));
+        System.out.println("spring.datasource.username=" + env.getProperty("spring.datasource.username"));
+        System.out.println("------------------------------------");
     }
 
 }
